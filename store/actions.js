d8dc243100c214da9266df2c716c9a4008feb152
Chris Stark <starkfree@gmail.com>
2021-02-27 16:49:20 -0500

    fix: fixes access token error message cookie

--- store/actions.js
+++ store/actions.js
@@ -3,21 +3,22 @@ import cookieparser from 'cookieparser'
 
 const actions = {
     // Action to be performed on server init
-    async nuxtServerInit ({ commit }, { req }) {
-        if (process.server && process.static ) return
-        if (!req.headers.cookie) return
+    nuxtServerInit ({ commit }, { req }) {
+        if (process.server && process.static) { return }
+        if (!req.headers.cookie) { return }
 
         const parsed = cookieparser.parse(req.headers.cookie)
         const accessTokenCookie = parsed.access_token
 
-        if (!accessTokenCookie) return
+        if (accessTokenCookie === 'null') {
+            return
+        }
 
         const decodedUser = jwtDecode(accessTokenCookie)
-        if (!decodedUser) return
+        if (!decodedUser) { return }
 
         // If we have a token, set it
-        commit( 'users/SET_USER', { user: decodedUser, token: accessTokenCookie })
-
+        commit('users/SET_USER', { user: decodedUser, token: accessTokenCookie })
     },
     toggleLoadingOverlay: ({ commit }) => {
         commit('TOGGLE_LOADING_OVERLAY')
